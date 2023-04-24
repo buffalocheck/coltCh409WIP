292dd1af845b9d40fb8e8ec94aa39c3c6006d574
Shawn Adams <shawn@buffalocheck.com>
2023-04-24 09:58:17 -0700

    Initial upload.
    Initial upload.

---
+++ Mongoose_Express-SA409 copy/index.js
@@ -0,0 +1,38 @@
+const express = require('express'); //ch 409.
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose'); //Mongoose connection stuff.
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand',
+    { useNewURLParser: true, useUnifiedTopology: true }) //Colt suggests this.
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!")
+        console.log(err)
+    }); //Mongoose connection stuff.
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+app.get('products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/show', { product })
+})
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000!!")
+})
